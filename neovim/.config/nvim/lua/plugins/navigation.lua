20f822cb4133480cb44f35b44f9f18b7baf93362
Amey Patil <ameypatil96@gmail.com>
2025-05-10 13:11:24 +0530

    Yazi and Optional Machine Specific settings

--- neovim/.config/nvim/lua/plugins/navigation.lua
+++ neovim/.config/nvim/lua/plugins/navigation.lua
@@ -2,18 +2,54 @@ return {
     ------------------------------------------------------------------------------------------------------------------
     -------------------------------------------- File Managers -------------------------------------------------------
     ------------------------------------------------------------------------------------------------------------------
+    -- {
+    --     "nvim-neo-tree/neo-tree.nvim",
+    --     branch = "v3.x",
+    --     dependencies = {
+    --         "nvim-lua/plenary.nvim",
+    --         "nvim-tree/nvim-web-devicons",
+    --         "MunifTanjim/nui.nvim",
+    --         "3rd/image.nvim",
+    --     },
+    --     keys = {
+    --         { "<F2>", "<cmd>Neotree toggle<CR>", mode={"n", "i", "v"}, desc="Toggle NeoTree Side-Pane" },
+    --     },
+    -- },
     {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
+        "mikavilpas/yazi.nvim",
+        event = "VeryLazy",
         dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons",
-            "MunifTanjim/nui.nvim",
-            "3rd/image.nvim",
+            "folke/snacks.nvim"
         },
         keys = {
-            { "<F2>", "<cmd>Neotree toggle<CR>", mode={"n", "i", "v"}, desc="Toggle NeoTree Side-Pane" },
+            {
+                "<leader>cc",
+                mode = { "n", "v" },
+                "<cmd>Yazi<cr>",
+                desc = "Open yazi at the current file",
+            },
+            {
+                -- Open in the current working directory
+                "<leader>cw",
+                "<cmd>Yazi cwd<cr>",
+                desc = "Open the file manager in nvim's working directory",
+            },
+            {
+                "<leader>ct",
+                "<cmd>Yazi toggle<cr>",
+                desc = "Resume the last yazi session",
+            },
         },
+        opts = {
+            -- if you want to open yazi instead of netrw, see below for more info
+            open_for_directories = false,
+            keymaps = {
+                show_help = "<f1>",
+            },
+        },
+        init = function()
+            vim.g.loaded_netrwPlugin = 1
+        end,
     },
     {
         'stevearc/oil.nvim',
@@ -37,7 +73,7 @@ return {
             "nvim-tree/nvim-web-devicons"
         },
         keys = {
-            -- { "<F3>", require('oil').toggle_float, mode={"n", "i", "v"}, desc="Toggle Oil FLoating Window" },
+            -- { "<F3>", require('oil').toggle_float, mode = { "n", "i", "v" }, desc = "Toggle Oil FLoating Window" },
         },
     },
 
@@ -47,29 +83,41 @@ return {
     {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.8',
-        lazy=false,
+        lazy = false,
         dependencies = {
             'nvim-lua/plenary.nvim',
             { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
         },
-        config = function ()
+        config = function()
             require('telescope').load_extension('fzf')
             -- Core Bindings
-            vim.keymap.set({"n", "v"}, "<leader>ff", ":Telescope find_files<cr>", {silent = true, noremap = true, desc="Telescope Find Files" })
-            vim.keymap.set({"n", "v"}, "<leader>fg", ":Telescope live_grep<cr>", {silent = true, noremap = true, desc="Telescope Live Grep" })
-            vim.keymap.set({"n", "v"}, "<leader>fb", ":Telescope buffers<cr>", { silent = true, noremap = true, desc="Telescope Buffers" })
-            vim.keymap.set({"n", "v"}, "<Leader>km", ":Telescope keymaps<cr>", { silent = true, noremap = true, desc="Show Keymaps" })
-            vim.keymap.set({"n", "v"}, "<leader>fh", ":Telescope help_tags<cr>", { silent = true, noremap = true, desc="Telescope Help Tags" })
+            vim.keymap.set({ "n", "v" }, "<leader>ff", ":Telescope find_files<cr>",
+                { silent = true, noremap = true, desc = "Telescope Find Files" })
+            vim.keymap.set({ "n", "v" }, "<leader>fg", ":Telescope live_grep<cr>",
+                { silent = true, noremap = true, desc = "Telescope Live Grep" })
+            vim.keymap.set({ "n", "v" }, "<leader>fb", ":Telescope buffers<cr>",
+                { silent = true, noremap = true, desc = "Telescope Buffers" })
+            vim.keymap.set({ "n", "v" }, "<Leader>km", ":Telescope keymaps<cr>",
+                { silent = true, noremap = true, desc = "Show Keymaps" })
+            vim.keymap.set({ "n", "v" }, "<leader>fh", ":Telescope help_tags<cr>",
+                { silent = true, noremap = true, desc = "Telescope Help Tags" })
 
             -- Git Bindings
-            vim.keymap.set({"n", "v"}, "<Leader>gc", ":Telescope git_commits<cr>", { silent = true, noremap = true, desc="List Git Commits" })
-            vim.keymap.set({"n", "v"}, "<Leader>gf", ":Telescope git_files<cr>", { silent = true, noremap = true, desc="List Git Files" })
-            vim.keymap.set({"n", "v"}, "<Leader>gs", ":Telescope git_stash<cr>", { silent = true, noremap = true, desc="List Git Stash" })
-            vim.keymap.set({"n", "v"}, "<Leader>gg", ":Telescope git_status<cr>", { silent = true, noremap = true, desc="Git Status" })
+            vim.keymap.set({ "n", "v" }, "<Leader>gc", ":Telescope git_commits<cr>",
+                { silent = true, noremap = true, desc = "List Git Commits" })
+            vim.keymap.set({ "n", "v" }, "<Leader>gf", ":Telescope git_files<cr>",
+                { silent = true, noremap = true, desc = "List Git Files" })
+            vim.keymap.set({ "n", "v" }, "<Leader>gs", ":Telescope git_stash<cr>",
+                { silent = true, noremap = true, desc = "List Git Stash" })
+            vim.keymap.set({ "n", "v" }, "<Leader>gg", ":Telescope git_status<cr>",
+                { silent = true, noremap = true, desc = "Git Status" })
 
             -- Diagnostics
-            vim.keymap.set({"n", "v"}, "<Leader>ce", ':lua require"telescope.builtin"diagnostics{ severity = "error" }<cr>', { silent = true, noremap = true, desc="Errors in Current Project" })
-            vim.keymap.set({"n", "v"}, "<Leader>cd", ':lua require"telescope.builtin"diagnostics{}<cr>', { silent = true, noremap = true, desc="List Git Commits" })
+            vim.keymap.set({ "n", "v" }, "<Leader>ce",
+                ':lua require"telescope.builtin"diagnostics{ severity = "error" }<cr>',
+                { silent = true, noremap = true, desc = "Errors in Current Project" })
+            vim.keymap.set({ "n", "v" }, "<Leader>cd", ':lua require"telescope.builtin"diagnostics{}<cr>',
+                { silent = true, noremap = true, desc = "List Git Commits" })
         end,
     },
     {
@@ -77,14 +125,14 @@ return {
         dependencies = {
             'nvim-telescope/telescope.nvim',
         },
-        lazy=false,
+        lazy = false,
         keys = {
-            { "<c-e>", "<cmd>Telescope symbols<cr>", mode={"i", "n", "v"}, desc="Telescope Symbols" },
+            { "<c-e>", "<cmd>Telescope symbols<cr>", mode = { "i", "n", "v" }, desc = "Telescope Symbols" },
         },
     },
     {
         'ahmedkhalf/project.nvim',
-        lazy=false,
+        lazy = false,
         dependencies = {
             'nvim-telescope/telescope.nvim',
         },
@@ -92,20 +140,26 @@ return {
             require('telescope').load_extension('projects')
         end,
         keys = {
-            { "<leader>fp", "<cmd>Telescope projects<cr>", mode={"n", "v"}, desc="Telescope Projects" },
+            { "<leader>fp", "<cmd>Telescope projects<cr>", mode = { "n", "v" }, desc = "Telescope Projects" },
         },
     },
     ------------------------------------------------------------------------------------------------------------------
     ------------------------------------------------ Miscellaneous ---------------------------------------------------
     ------------------------------------------------------------------------------------------------------------------
     {
-          "hedyhli/outline.nvim",
-          lazy = false,
-          keys = {
-              { "<leader>o", "<cmd>Outline<cr>", mode={"n", "v"}, desc="Outline" },
-          },
-          config = function ()
-              require("outline").setup()
-          end,
+        "ggandor/leap.nvim",
+        config = function()
+            require("leap").set_default_mappings()
+        end
+    },
+    {
+        "hedyhli/outline.nvim",
+        lazy = false,
+        keys = {
+            { "<leader>o", "<cmd>Outline<cr>", mode = { "n", "v" }, desc = "Outline" },
+        },
+        config = function()
+            require("outline").setup()
+        end,
     }
 }
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -98,6 +98,7 @@ HIST_STAMPS="dd/mm/yyyy"
 plugins=(
 	git
 	fzf
+    vi-mode
 )
 
 source $ZSH/oh-my-zsh.sh
@@ -122,6 +123,21 @@ activate() {
 
 # Zoxide setup
 eval "$(zoxide init zsh)"
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('$HOME/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="$HOME/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
 
 
 # Source additional config if exists
