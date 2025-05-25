34306fd4682dbfe5166821a6763b642844f4d0c4
Amey Patil <ameypatil96@gmail.com>
2025-05-25 21:08:31 +0530

    Minor errors.
    Not assuming a preinstalled python distribution.

--- scripts/powerline.sh
+++ scripts/powerline.sh
@@ -3,4 +3,11 @@ mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
 
 # Powerline Status
-pip install powerline-status
+if pip --version; then
+    python -m pip install powerline-status
+elif pip3 --version; then
+    python3 -m pip install powerline-status
+else
+    echo "Neither `pip` nor `pip3` found"
+    exit 1
+fi
--- scripts/rust.sh
+++ scripts/rust.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
-echo $(cargo --version)
 if ! cargo --version; then
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 	source ~/.cargo/env
 fi
 
-cargo install exa bat fd-find zoxide macchina
-cargo install --locked nu yazi-fm yazi-cli
-
+cargo install --locked nu exa bat fd-find zoxide macchina
+# cargo install --locked yazi-fm yazi-cli  # Installed Using SNAP
