dbd4758b106bc6d66d644f2ead48feea51483516
Amey Patil <ameypatil96@gmail.com>
2025-05-25 23:45:56 +0530

    TMUX Powerline update

--- scripts/powerline.sh
+++ scripts/powerline.sh
@@ -1,13 +1,3 @@
 # NerdFonts
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
-
-# Powerline Status
-if pip --version; then
-    python -m pip install powerline-status
-elif pip3 --version; then
-    python3 -m pip install powerline-status
-else
-    echo "Neither `pip` nor `pip3` found"
-    exit 1
-fi
